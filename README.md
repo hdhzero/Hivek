219f6dd69155503c5f61f30816670335b60e23eb
hdhzero <hdhzero@hunter.(none)>
2013-03-01 18:34:00 -0300

    renaming project from Ivek to Hivek

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-ivek
+Hivek
 ====
 
-Ivek is a VLIW soft processor written in VHDL with a variable
+Hivek is a VLIW soft processor written in VHDL with a variable
 length instruction set and capable of executing at most two instructions
 in parallel.
--- src/hivek.vhd
+++ src/hivek.vhd
@@ -2,13 +2,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity ikev is
+entity hivek is
     port (
         clock : in std_logic;
         reset : in std_logic
     );
 end ikev;
 
-architecture ikev_arch of ikev is
+architecture hivek_arch of hivek is
 begin
-end ikev_arch;
+end hivek_arch;
