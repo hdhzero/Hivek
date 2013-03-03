cb534cf4765e442bdfb018f30bcf86bee03199b4
hdhzero <hdhzero@hunter.(none)>
2013-03-03 00:34:11 -0300

    creating the hivek package

---
+++ src/vhdl/hivek_pack.vhd
@@ -0,0 +1,62 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package hiveck_pack is
+    ------------------------
+    -- constants definitions
+    ------------------------
+
+    constant ZERO : std_logic_vector(63 downto 0) := x"0000000000000000";
+    constant ONES : std_logic_vector(63 downto 0) := x"FFFFFFFFFFFFFFFF";
+    constant ONE  : std_logic_vector(63 downto 0) := x"0000000000000001";
+
+    -------------------------
+    -- components definitions
+    -------------------------
+
+    -- used in the register bank
+    component load_decoder is
+    port (
+        address : in std_logic_vector(3 downto 0);
+        loads   : out std_logic_vector(15 downto 0)
+    );
+    end component;
+
+    -- used in the register bank
+    component single_register is
+    port (
+        clock : in std_logic;
+        reset : in std_logic;
+        load1 : in std_logic;
+        load2 : in std_logic;
+        din1  : in std_logic_vector(31 downto 0);
+        din2  : in std_logic_vector(31 downto 0);
+        dout  : out std_logic_vector(31 downto 0)
+    );
+    end component;
+
+    -- the register bank
+    component register_bank is
+    port (
+        clock   : in std_logic;
+        reset   : in std_logic;
+        load1   : in std_logic;
+        load2   : in std_logic;
+        reg_a1  : in std_logic_vector(3 downto 0);
+        reg_b1  : in std_logic_vector(3 downto 0);
+        reg_a2  : in std_logic_vector(3 downto 0);
+        reg_b2  : in std_logic_vector(3 downto 0);
+        reg_c1  : in std_logic_vector(3 downto 0);
+        reg_c1  : in std_logic_vector(3 downto 0);
+        din_c1  : in std_logic_vector(31 downto 0);
+        din_c2  : in std_logic_vector(31 downto 0);
+        dout_a1 : out std_logic_vector(31 downto 0);
+        dout_b1 : out std_logic_vector(31 downto 0);
+        dout_a2 : out std_logic_vector(31 downto 0);
+        dout_b2 : out std_logic_vector(31 downto 0)
+    );
+    end component;
+
+
+end package;
